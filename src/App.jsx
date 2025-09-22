b1a28e7d49bc2aba98af2993b77a563049438c08
Miguel Pineda <realmadrid.mi9u31@gmail.com>
2025-09-21 23:26:02 -0600

    Integracion API Booking, Frontend actualizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import appFirebase from "./credenciales";
